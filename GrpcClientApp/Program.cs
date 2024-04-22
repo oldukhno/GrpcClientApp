9e370bb156974b611ae41f461a84fefe5e7bf698
Oleksii Dukhno <oldukhno@microsoft.com>
2024-04-22 06:47:20 -0700

    added autofac

--- GrpcClientApp/Program.cs
+++ GrpcClientApp/Program.cs
@@ -1,24 +1,23 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Grpc.Core;
 using Grpc.Net.ClientFactory;
 using GrpcService;
 
 var builder = WebApplication.CreateBuilder(args);
 
+const string Name = "Greeter";
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services
-    .AddGrpcClient<Greeter.GreeterClient>("Greeter", options =>
-    {
-        options.Address = new Uri("http://localhost:5159");
-        options.ChannelOptionsActions.Add(channelOptions =>
-        {
-            channelOptions.Credentials = ChannelCredentials.Insecure;
-        });
-    });
 
 var app = builder.Build();
+
+var containerBuilder = new ContainerBuilder();
+
+var rootContainer = containerBuilder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,11 +30,30 @@ var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
 
 app.MapGet("/greetings", () =>
     {
-        using var scope = scopeFactory.CreateScope();
-        var grpcClientFactory = scope.ServiceProvider.GetRequiredService<GrpcClientFactory>();
-        var grpcClient = grpcClientFactory.CreateClient<Greeter.GreeterClient>("Greeter");
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddGrpcClient<Greeter.GreeterClient>(Name, options =>
+            {
+                options.Address = new Uri("http://localhost:5160");
+                options.ChannelOptionsActions.Add(channelOptions =>
+                {
+                    channelOptions.Credentials = ChannelCredentials.Insecure;
+                });
+            });
+
+        var scope = rootContainer.BeginLifetimeScope(builder =>
+        {
+            builder.Populate(serviceCollection);
+        });
+
+        var grpcClientFactory = scope.Resolve<GrpcClientFactory>();
+        var grpcClient = grpcClientFactory.CreateClient<Greeter.GreeterClient>(Name);
 
         var reply = grpcClient.SayHello(new HelloRequest { Name = "Neo" });
+
+        scope.Dispose();
+
+        // check that we did not dispose the channels here
+
         return reply.Message;
     })
     .WithName("GetGreetings")
